876c73a670de66aec12aa07ecf2cd02894e1b8a4
yasminalmousa <almousa.y@northeastern.edu>
2025-06-21 17:59:16 -0400

    Creation of project and main class
    Update

---
+++ TeamGreeting/src/Model/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+public class Main {
+    public static void main(String[] args) {
+        Member1.main(args);
+        Member2.main(args);
+        Member3.main(args);
+        Member4.main(args);
+    }
+}
