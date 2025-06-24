345b1e8c5920be9310ff740d24c0b91b4325f198
yasminalmousa <almousa.y@northeastern.edu>
2025-06-23 22:55:49 -0400

    Complete merge and code success
    update

--- TeamGreeting/src/Model/Main.java
+++ TeamGreeting/src/Model/Main.java
@@ -6,9 +6,9 @@ package Model;
 
 public class Main {
     public static void main(String[] args) {
-        //Member1.main(args);
+        Member1.main(args);
         Member2.main(args);
-        //Member3.main(args);
+        Member3.main(args);
         //Member4.main(args);
     }
 }
