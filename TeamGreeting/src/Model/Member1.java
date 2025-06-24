85bb4ffe6be8b4500c43aced130ffb2f17df5e75
Bencoff2002 <kwofie.p@northeastern.edu>
2025-06-23 22:22:48 -0400

    Create member 1
    Update

---
+++ TeamGreeting/src/Model/Member1.java
@@ -0,0 +1,15 @@
+/*
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
+package Model;
+ 
+/**
+*
+* @author Paul
+*/
+public class Member1 {
+    public static void main(String[] args) {
+    System.out.println("Hello from Member 1, Paul!");
+    }
+}
