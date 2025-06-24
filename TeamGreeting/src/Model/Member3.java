ff720c38f322258904d0b1c1bef5b5a3270d8160
Tolu Mebaanne <mebaanne.t@northeastern.edu>
2025-06-23 22:22:45 -0400

    Create Member3
    Update to member3 class

---
+++ TeamGreeting/src/Model/Member3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Tolu
+ */
+public class Member3 {
+    public static void main(String[] args) {
+        System.out.println("Hello from Member 3, Toluwalase!");
+    }
+}
